58259dfe6d4d23258a0cc3ff7e8f7fe32f08286e
TeisRaul <147483530+TeisRaul@users.noreply.github.com>
2024-11-29 02:23:02 +0200

    my first tensor, movin it to gpu and moving back on cpu

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import torch
 # Count number of devices
 if torch.cuda.is_available():
-    print(torch.cuda.device_count())
+    device = "cuda" # Use NVIDIA GPU (if available)
+elif torch.backends.mps.is_available():
+    device = "mps" # Use Apple Silicon GPU (if available)
 else:
-    print(0)
+    device = "cpu" # Default to CPU if no GPU is available
+print(device)
+
+# Create tensor (default on CPU)
+tensor = torch.tensor([1, 2, 3])
+
+# Tensor not on GPU
+print(tensor, tensor.device)
+
+# Move tensor to GPU (if available)
+tensor_on_gpu = tensor.to(device)
+print(tensor_on_gpu, tensor_on_gpu.device)
+
+# Instead, copy the tensor back to cpu
+tensor_back_on_cpu = tensor_on_gpu.cpu().numpy()
+print(tensor_back_on_cpu)
